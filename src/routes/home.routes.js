39b38110449f61cc6a15d621830d1d4695824ce3
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-02-13 22:51:36 -0400

    style: format files routes

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -2,22 +2,25 @@ import * as React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {useTheme} from 'react-native-paper';
 import HomeScreen from '../pages/index';
+import Reader from '../pages/reader';
 
 const Stack = createStackNavigator();
 
 const HomeRoute = ({title}) => {
   const {colors, fonts} = useTheme();
+  const options = {
+    headerStyle: {
+      backgroundColor: colors.backdrop,
+    },
+    headerTitleAlign: 'center',
+    headerTintColor: colors.primary,
+    headerTitleStyle: fonts.medium,
+  };
+
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.backdrop,
-        },
-        headerTitleAlign: 'center',
-        headerTintColor: colors.primary,
-        headerTitleStyle: fonts.medium,
-      }}>
+    <Stack.Navigator screenOptions={options}>
       <Stack.Screen name="Home" component={HomeScreen} options={{title}} />
+      <Stack.Screen name="Reader" component={Reader} options={{title}} />
     </Stack.Navigator>
   );
 };
